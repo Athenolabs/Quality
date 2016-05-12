623916793ed67cd121f2c4924712575b0e8f8664
creador30 <creador@abwesend.de>
2016-05-12 09:54:48 -0700

    Update

---
+++ quality/quality/doctype/initial_sample_inspection_report/initial_sample_inspection_report.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, ESO Electronic (Nemwatik) and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Initial Sample Inspection Report', {
+	refresh: function(frm) {
+
+	}
+});
---
+++ quality/quality/doctype/standards_and_norms/standards_and_norms.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2016, ESO Electronic (Nemwatik) and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Standards and Norms', {
+	refresh: function(frm) {
+
+	}
+});
